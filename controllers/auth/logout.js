8e6d467fcd81cccaf517b389a7e15c85765de3e4
Oleksii Verbytskyi <alverba88@gmail.com>
2022-07-31 18:11:27 +0300

    finished task-4

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,9 +1,9 @@
-const bcrypt = require("bcryptjs");
 const {User} = require('../../models/user');
-const {createError} = require('../../helpers/createError');
 
 const logout = async (req, res) => {
-
+    const {_id} = req.user;
+    await User.findByIdAndUpdate(_id, {token: ""});
+    res.status(204).json();
 }
 
 module.exports = logout;
--- models/user.js
+++ models/user.js
@@ -7,10 +7,12 @@ const userSchema = new Schema(
     {
         password: {
             type: String,
+            minlength: 6,
             required: [true, 'Password is required'],
         },
         email: {
             type: String,
+            match: regex.emailRegex,
             required: [true, 'Email is required'],
             unique: true,
         },
@@ -27,8 +29,28 @@ const userSchema = new Schema(
 
 const User = model("user", userSchema);
 
+const registerSchema = Joi.object({
 
-const schemas={};
+    email: Joi.string().pattern(regex.emailRegex).required(),
+    password: Joi.string().min(6).required(),
+    subscription: Joi.string().valid("starter", "pro", "business"),
+    token: Joi.string(),
+});
+
+const loginSchema = Joi.object({
+    email: Joi.string().pattern(regex.emailRegex).required(),
+    password: Joi.string().min(6).required(),
+});
+
+const updateSubscriptionSchema = Joi.object({
+    subscription: Joi.string().valid("starter", "pro", "business"),
+});
+
+const schemas={
+    register: registerSchema,
+    login: loginSchema,
+    update: updateSubscriptionSchema
+};
 
 module.exports ={
     schemas,
